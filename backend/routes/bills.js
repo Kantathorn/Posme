d02aa5b5e855e1cb9595e816e4459bb4dc7640f6
PhumipatPeamlue <107468576+PhumipatPeamlue@users.noreply.github.com>
2022-09-18 13:44:08 +0700

    create GET method for get bill

--- backend/routes/bills.js
+++ backend/routes/bills.js
@@ -25,4 +25,29 @@ router.post('/', async (req, res) => {
   }
 });
 
+// get bill
+router.get('/', async (req, res) => {
+  try {
+    let { receipt_no, date } = req.query;
+
+    if (receipt_no) {
+      const bill = await Bill.findOne({ _id: receipt_no }).populate('user_id');
+      res.json(bill);
+    } else if (date) {
+      date = new Date(date);
+      const bills = await Bill.find({user_id: req.user._id}).populate('user_id');
+      const result = bills.filter(bill => {
+        const { time } = bill;
+        return time.getFullYear() === date.getFullYear() && time.getMonth()+1 === date.getMonth()+1 && time.getDate() === date.getDate()
+      });
+      res.json(result)
+    }
+    const bills = await Bill.find( {user_id: req.user._id} ).populate('user_id');
+    res.json(bills);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
